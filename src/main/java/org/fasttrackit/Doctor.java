a2cf9c9714f729f9ef023199aa5220b0117f5896
Krisztian Kakasi <cristy11_1998@yahoo.com>
2019-09-11 21:00:43 +0300

    Added 2 parent classes + Every class has itțs own properties

---
+++ src/main/java/org/fasttrackit/Doctor.java
@@ -0,0 +1,12 @@
+package org.fasttrackit;
+
+public class Doctor {
+    String name;
+    String doctorSpeciality;
+
+    //optional
+    boolean otherClients;
+    boolean availability;
+    double priceForVetServices;
+
+}
--- src/main/java/org/fasttrackit/VeterinaryDoctor.java
+++ src/main/java/org/fasttrackit/VeterinaryDoctor.java
@@ -1,17 +1,8 @@
 package org.fasttrackit;
 
-public class VeterinaryDoctor {
-    String name;
-    String doctorSpeciality;
-
-    //optional
-    boolean otherClients;
-    boolean availability;
-    double priceForVetServices;
+public class VeterinaryDoctor extends Doctor {
     int abilityToCalmTheAnimal;
     // If this ability is > than the scaredLevel of the animal then he can treat the animal
-
-
 
 
 
